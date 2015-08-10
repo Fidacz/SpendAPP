d7d83ce42a8cf3b2d86d976aa59f76bfcf538ee8
fida <fidacz@gmail.com>
2015-08-10 20:19:57 +0200

    finishing SQLHelper class

--- app/src/main/java/com/example/filip/spendapp/SQLHelper.java
+++ app/src/main/java/com/example/filip/spendapp/SQLHelper.java
@@ -1,8 +1,12 @@
 package com.example.filip.spendapp;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+
+import java.util.AbstractList;
+import java.util.ArrayList;
 
 /**
  * Created by fida on 9.8.15.
@@ -15,7 +19,7 @@ public class SQLHelper extends SQLiteOpenHelper{
 
 
     protected static final String DATABASE_NAME = "spendApp";
-    protected static final int DATABASE_VERSION = 2;
+    protected static final int DATABASE_VERSION = 1;
     protected static final String TB_NAME = "Transaction";
 
 
@@ -27,16 +31,85 @@ public class SQLHelper extends SQLiteOpenHelper{
     public void onCreate(SQLiteDatabase db) {
         // Vytvoreni tabulky
         db.execSQL("CREATE TABLE" + TB_NAME + "( ID INTEGER PRIMARY KEY" +
-                "VALUE INTEGER NOT NULL" +
-                "DATE TEXT NOT NULL" +
-                "COMENT TEXT" +
-                "CATEGORY TEXT NOT NULL" +
-                "TYPE INTEGER NOT NULL);");
+                "Value DOUBLE NOT NULL" +
+                "Date TEXT NOT NULL" +
+                "Coment TEXT" +
+                "Category TEXT NOT NULL" +
+                "Type INTEGER NOT NULL);");
 
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 
+    }
+
+
+    public void addTransaction (Transaction transaction){
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        db.execSQL("INSERT INTO" + TB_NAME + "(ID, Value, Date, Coment, Category, Type)" +
+                "VALUES (" + transaction.getId() +
+                            transaction.getValue() +
+                            transaction.getDate() +
+                            transaction.getComment() +
+                            transaction.getCategory() +
+                            transaction.getType() + ")");
+        db.close();
+    }
+
+    public void delteTransaction(int ID){
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        db.execSQL("DELETE FROM" + TB_NAME +
+        "WHERE ID=" + ID);
+
+        db.close();
+    }
+
+
+
+    public Transaction getTransaction (int ID){
+
+        SQLiteDatabase db = this.getWritableDatabase();
+        Transaction transaction = new Transaction();
+
+        Cursor cursor = db.rawQuery("SELECT * FROM" + TB_NAME + "WHERE ID=" + ID, null);
+
+        transaction.setId(Integer.valueOf(cursor.getString(0)));
+        transaction.setValue(Double.valueOf(cursor.getString(1)));
+        transaction.setDate(cursor.getString(2));
+        transaction.setComent(cursor.getString(3));
+        transaction.setCategory(cursor.getString(4));
+        transaction.setType(Integer.valueOf(cursor.getString(5)));
+
+
+        db.close();
+        return transaction;
+    }
+
+
+    public ArrayList<Transaction> getTransactions (){
+
+        SQLiteDatabase db = this.getWritableDatabase();
+        ArrayList<Transaction> transactions = new ArrayList<>();
+
+        Cursor cursor = db.rawQuery("SELECT * FROM" + TB_NAME , null);
+
+        if (cursor.moveToFirst()) {
+            do {
+                Transaction transaction = new Transaction();
+                transaction.setId(Integer.valueOf(cursor.getString(0)));
+                transaction.setValue(Double.valueOf(cursor.getString(1)));
+                transaction.setDate(cursor.getString(2));
+                transaction.setComent(cursor.getString(3));
+                transaction.setCategory(cursor.getString(4));
+                transaction.setType(Integer.valueOf(cursor.getString(5)));
+                transactions.add(transaction);
+            } while (cursor.moveToNext());
+        }
+
+        db.close();
+        return transactions;
     }
 }
